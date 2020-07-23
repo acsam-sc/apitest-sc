12ab16c7e9a5120fe7f34096c13f06fc725d23b0
Anton <acsam42@gmail.com>
2020-07-23 17:23:03 +0200

    another commit

--- client/components/home.js
+++ client/components/home.js
@@ -1,5 +1,38 @@
 import React from 'react'
+import { Link, Route, useParams } from 'react-router-dom'
 import Header from './header'
+
+const Dashboard = () => {
+  return (
+    <div>
+      <div id="title">Dashboard</div>
+      <Link to="/dashboard/profile/15ef5221-bff1-11e9-954d-2f6ce28e9166">Go To Profile</Link>
+      <Link to="/dashboard/main">Go To Main</Link>
+    </div>
+  )
+}
+
+const DashboardMain = () => {
+  return (
+    <div>
+      <div id="title">Dashboard</div>
+      <Link to="/dashboard/profile/15ef5221-bff1-11e9-954d-2f6ce28e9166">Go To Profile</Link>
+      <Link to="/dashboard">Go To Root</Link>
+    </div>
+  )
+}
+
+const ProfileUser = () => {
+  const { user } = useParams()
+  return (
+    <div>
+      <div id="title">Profile</div>
+      <div id="username">{user}</div>
+      <Link to="/dashboard">Go To Root</Link>
+      <Link to="/dashboard/main">Go To Main</Link>
+    </div>
+  )
+}
 
 const Home = () => {
   return (
@@ -7,7 +40,9 @@ const Home = () => {
       <Header />
       <div className="flex items-center justify-center h-screen">
         <div className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10">
-          {/* add your routes here */}
+          <Route exact path="/dashboard/" component={() => <Dashboard />} />
+          <Route exact path="/dashboard/main" component={() => <DashboardMain />} />
+          <Route exact path="/dashboard/profile/:user" component={() => <ProfileUser />} />
         </div>
       </div>
     </div>
--- server/server.js
+++ server/server.js
@@ -4,8 +4,12 @@ import path from 'path'
 import cors from 'cors'
 import bodyParser from 'body-parser'
 
+import axios from 'axios'
+
 import cookieParser from 'cookie-parser'
 import Html from '../client/html'
+
+const { readFile, writeFile, unlink } = require('fs').promises
 
 const port = process.env.PORT || 3000
 const server = express()
@@ -17,6 +21,85 @@ server.use(bodyParser.urlencoded({ limit: '50mb', extended: true, parameterLimit
 server.use(bodyParser.json({ limit: '50mb', extended: true }))
 
 server.use(cookieParser())
+
+const usersFile = `${__dirname}/users.json`
+
+const writeUsersFile = async (text) =>
+  writeFile(usersFile, JSON.stringify(text), { encoding: 'utf8' })
+
+const deleteUsersFile = async () => unlink(usersFile)
+
+const readUsersFile = async () => {
+  const fd = await readFile(usersFile, { encoding: 'utf8' })
+    .then((text) => JSON.parse(text))
+    .catch((err) => {
+      if (err.code === 'ENOENT') {
+        const resData = axios('https://jsonplaceholder.typicode.com/users')
+          .then(({ data }) => {
+            writeUsersFile(data)
+            return data
+          })
+          // eslint-disable-next-line no-console
+          .catch((errAxios) => console.log(`Error in axios: ${errAxios}`))
+        return resData
+      }
+      return err
+    })
+  return fd
+}
+
+server.use('/api/v1/*', (req, res, next) => {
+  res.set('x-skillcrucial-user', '15ef5221-bff1-11e9-954d-2f6ce28e9166')
+  res.set('Access-Control-Expose-Headers', 'X-SKILLCRUCIAL-USER')
+  next()
+})
+
+server.get('/api/v1/users', async (req, res) => {
+  const usersData = await readUsersFile()
+  // res.set('x-skillcrucial-user', '15ef5221-bff1-11e9-954d-2f6ce28e9166')
+  // res.set('Access-Control-Expose-Headers', 'X-SKILLCRUCIAL-USER')
+  res.json(usersData)
+})
+
+server.post('/api/v1/users', async (req, res) => {
+  const usersData = await readUsersFile()
+  const newUserId = usersData[usersData.length - 1].id + 1
+  const dataToWrite = [...usersData, { id: newUserId, ...req.body }]
+  writeUsersFile(dataToWrite)
+  const responseBody = { status: 'success', id: newUserId }
+  res.json(responseBody)
+})
+
+server.patch('/api/v1/users/:userId', async (req, res) => {
+  const usersData = await readUsersFile()
+  const newUserId = parseInt(req.params.userId, 10)
+  // const dataToWrite = [...usersData, { id: newUserId, ...req.body }]
+  const dataToWrite = usersData.map((it) => {
+    if (it.id === newUserId) {
+      // {id:1, name: "Leanne Graham", username: "Bret"} { name: "Elvis", surname: "Presley" }
+      return { ...it, ...req.body }
+    }
+    return it
+  })
+  writeUsersFile(dataToWrite)
+  const responseBody = { status: 'success', id: newUserId }
+  res.json(responseBody)
+})
+
+server.delete('/api/v1/users', async (req, res) => {
+  deleteUsersFile()
+  const responseBody = { status: 'success' }
+  res.json(responseBody)
+})
+
+server.delete('/api/v1/users/:userId', async (req, res) => {
+  const usersData = await readUsersFile()
+  const userIdToDelete = req.params.userId
+  const dataToWrite = usersData.filter((it) => it.id !== parseInt(userIdToDelete, 10))
+  writeUsersFile(dataToWrite)
+  const responseBody = { status: 'success', id: userIdToDelete }
+  res.json(responseBody)
+})
 
 server.use('/api/', (req, res) => {
   res.status(404)
@@ -49,4 +132,5 @@ server.get('/*', (req, res) => {
 
 server.listen(port)
 
+// eslint-disable-next-line no-console
 console.log(`Serving at http://localhost:${port}`)
